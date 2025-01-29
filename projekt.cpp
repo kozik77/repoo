6783dc95bf9f9773438c17fd047869efc682b44b
kozik77 <kozik@mat.umk.pl>
2025-01-29 02:26:19 +0100

    Dodano sprawdzanie, czy liczba jest dodatnia

--- projekt.cpp
+++ projekt.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 using namespace std;
 
+bool czy_pierwsza(int liczba) {
+    if (liczba <= 1) return false;
+    for (int i = 2; i * i <= liczba; ++i) {
+        if (liczba % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
     int liczba;
     cin >> liczba;
+
+    if (liczba <= 0) {
+        cout << "Liczba musi być dodatnia!" << endl;
+        return 1;
+    }
+
     cout << "Wczytana liczba: " << liczba << endl;
+    if (czy_pierwsza(liczba)) {
+        cout << "Liczba jest pierwsza." << endl;
+    } else {
+        cout << "Liczba nie jest pierwsza." << endl;
+    }
     return 0;
 }
