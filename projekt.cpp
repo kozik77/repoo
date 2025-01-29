4dbb7eb9a916dc01ba749e5be8dd8ed970bfb7d3
kozik77 <kozik@mat.umk.pl>
2025-01-29 02:25:36 +0100

    Dodano funkcję czy_pierwsza

--- projekt.cpp
+++ projekt.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
 
+bool czy_pierwsza(int liczba) {
+    if (liczba <= 1) return false;
+    for (int i = 2; i * i <= liczba; ++i) {
+        if (liczba % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
     int liczba;
     cin >> liczba;
     cout << "Wczytana liczba: " << liczba << endl;
+    if (czy_pierwsza(liczba)) {
+        cout << "Liczba jest pierwsza." << endl;
+    } else {
+        cout << "Liczba nie jest pierwsza." << endl;
+    }
     return 0;
 }
